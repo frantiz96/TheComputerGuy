bc7639e7363d80b978e25b032eb5180370a01c2d
Tiziano Franza <t.franza.fg@gmail.com>
2019-04-07 15:39:44 +0200

    Create userfunctions.c

---
+++ resources/userfunctions.c
@@ -0,0 +1,108 @@
+   /*******************************************************/
+   /*      "C" Language Integrated Production System      */
+   /*                                                     */
+   /*               CLIPS Version 6.30  08/16/14          */
+   /*                                                     */
+   /*                USER FUNCTIONS MODULE                */
+   /*******************************************************/
+
+/*************************************************************/
+/* Purpose:                                                  */
+/*                                                           */
+/* Principal Programmer(s):                                  */
+/*      Gary D. Riley                                        */
+/*                                                           */
+/* Contributing Programmer(s):                               */
+/*                                                           */
+/* Revision History:                                         */
+/*                                                           */
+/*      6.24: Created file to seperate UserFunctions and     */
+/*            EnvUserFunctions from main.c.                  */
+/*                                                           */
+/*      6.30: Removed conditional code for unsupported       */
+/*            compilers/operating systems (IBM_MCW,          */
+/*            MAC_MCW, and IBM_TBC).                         */
+/*                                                           */
+/*************************************************************/
+
+/***************************************************************************/
+/*                                                                         */
+/* Permission is hereby granted, free of charge, to any person obtaining   */
+/* a copy of this software and associated documentation files (the         */
+/* "Software"), to deal in the Software without restriction, including     */
+/* without limitation the rights to use, copy, modify, merge, publish,     */
+/* distribute, and/or sell copies of the Software, and to permit persons   */
+/* to whom the Software is furnished to do so.                             */
+/*                                                                         */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS */
+/* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF              */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT   */
+/* OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY  */
+/* CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES */
+/* WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN   */
+/* ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF */
+/* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.          */
+/*                                                                         */
+/***************************************************************************/
+
+#include "clips.h"
+
+void UserFunctions(void);
+void EnvUserFunctions(void *);
+
+/*********************************************************/
+/* UserFunctions: Informs the expert system environment  */
+/*   of any user defined functions. In the default case, */
+/*   there are no user defined functions. To define      */
+/*   functions, either this function must be replaced by */
+/*   a function with the same name within this file, or  */
+/*   this function can be deleted from this file and     */
+/*   included in another file.                           */
+/*********************************************************/
+void UserFunctions()
+  {
+   // Use of UserFunctions is deprecated.
+   // Use EnvUserFunctions instead.
+  }
+
+void* load_description(void *environment) {
+    const char* s = EnvRtnLexeme(environment, 0);
+    char label[256];
+	char description[4096];
+
+	FILE* file = fopen("../resources/configurations.txt","r");
+	char buffer[1024];
+
+	while (!feof(file)) {
+		fscanf(file, "%[^#]", buffer);
+		fgetc(file);
+		fscanf(file, "%[^\n]", label);
+		fscanf(file, "%[^#]", description);
+
+		if (strcmp(label,s)==0) {
+			fclose(file);
+			return EnvAddSymbol(environment, description);
+		}
+	}
+
+	return EnvAddSymbol(environment, "STRINGA NON TROVATA");
+}
+  
+/***********************************************************/
+/* EnvUserFunctions: Informs the expert system environment */
+/*   of any user defined functions. In the default case,   */
+/*   there are no user defined functions. To define        */
+/*   functions, either this function must be replaced by   */
+/*   a function with the same name within this file, or    */
+/*   this function can be deleted from this file and       */
+/*   included in another file.                             */
+/***********************************************************/
+void EnvUserFunctions(
+  void *environment)
+  {
+#if MAC_XCD
+#pragma unused(environment)
+#endif
+	EnvDefineFunction(environment, "load_description", 's', PTIEF load_description, "load_description");
+  }
+
